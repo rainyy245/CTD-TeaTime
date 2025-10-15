2ef0e2ec7ba45797a5cd9a8f3df837d428ff720d
rainyy245 <raineewrn@gmail.com>
2025-10-15 16:56:18 +0800

    btn

--- home.py
+++ home.py
@@ -1,2 +1,7 @@
 import streamlit as st
 import ngrok as ng
+
+st.title("Welcome to CTD-TeaTime!")
+x = st.text_input("Favourite Tea?")
+st.write (f"Your favourite tea is :{x}") 
+is_cliceked = st.button("Click Me") 
