cee2540b7458821545ce96e76afb418ddf8b3a06
rainyy245 <raineewrn@gmail.com>
2025-10-20 15:10:50 +0800

    Add files via upload

--- home.py
+++ home.py
@@ -1,29 +1,31 @@
 import streamlit as st
-import ngrok as ng
 
-st.title("Welcome to CTD-TeaTime!")
+
+
+st.set_page_config("TeaTime", page_icon="☕", layout="wide")
+st.title("☕ Welcome to CTD-TeaTime!")
 st.header("Need Some Tea?")
 if st.button("Start Ordering"):
         st.switch_page("pages/1_menu.py")
 
-st.markdown("""
-<style>
-    .main {
-        padding: 2rem;
-    }
-    .stButton>button {
-        width: 100%;
-        background-color: #FF6B6B;
-        color: white;
-        font-weight: bold;
-        padding: 0.75rem;
-        border-radius: 10px;
-        border: none;
-        font-size: 1.1rem;
-    }
-    .stButton>button:hover {
-        background-color: #FF5252;
-    }
-            </style>
-""", unsafe_allow_html=True)
+# st.markdown("""
+# <style>
+#     .main {
+#         padding: 2rem;
+#     }
+#     .stButton>button {
+#         width: 100%;
+#         background-color: #FF6B6B;
+#         color: white;
+#         font-weight: bold;
+#         padding: 0.75rem;
+#         border-radius: 10px;
+#         border: none;
+#         font-size: 1.1rem;
+#     }
+#     .stButton>button:hover {
+#         background-color: #FF5252;
+#     }
+#             </style>
+# """, unsafe_allow_html=True)
 
